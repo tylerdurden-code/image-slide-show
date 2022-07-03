a9bc1345d40105726d413e9292c6cca98c8c9cf9
Tyler Durden <tylerdurden1412@gmail.com>
2022-07-03 18:21:36 +0300

    add bullet navigation

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 const frame = document.querySelector('.frame');
 const nextBtn = document.querySelector('[data-next-button]');
 const previousBtn = document.querySelector('[data-previous-button]');
+const dots = document.querySelector('[data-dots]');
 
 const myImages = [
-  { name: 'astronaut', imgUrl: 'img/astronaut.jpg' },
-  { name: 'cyberpunk', imgUrl: 'img/cyberpunk.jpg' },
-  { name: 'forest1', imgUrl: 'img/forest1.jpg' },
-  { name: 'mountain', imgUrl: 'img/mountain.jpg' },
-  { name: 'retro', imgUrl: 'img/retro.jpg' },
+  { name: 'astronaut', imgUrl: 'img/astronaut.jpg', active: true },
+  { name: 'cyberpunk', imgUrl: 'img/cyberpunk.jpg', active: false },
+  { name: 'forest1', imgUrl: 'img/forest1.jpg', active: false },
+  { name: 'mountain', imgUrl: 'img/mountain.jpg', active: false },
+  { name: 'retro', imgUrl: 'img/retro.jpg', active: false },
 ];
+
+function makeActive() {
+  myImages.forEach((img) => {
+    if (img.active === false) {
+      const dot = document.querySelector(`[data-image-attached = "${img.name}"]`);
+      console.log(dot, img.name);
+      if (dot.classList.contains('active')) {
+        dot.classList.remove('active');
+        return;
+      }
+    }
+    if (img.active === true) {
+      const dot = document.querySelector(`[data-image-attached = "${img.name}"]`);
+      console.log(dot, img.name);
+      dot.classList.add('active');
+    }
+  });
+}
+function appendBullets() {
+  for (let i = 0; i < myImages.length; i += 1) {
+    const dot = document.createElement('div');
+    if (i === 0) {
+      dot.classList.add('dot');
+      dot.dataset.imageAttached = myImages[i].name;
+    } else {
+      dot.classList.add('dot');
+      dot.dataset.imageAttached = myImages[i].name;
+    }
+    dots.appendChild(dot);
+  }
+}
+
 function appendImg(imgUrl) {
   const img = document.createElement('img');
   img.setAttribute('src', imgUrl);
@@ -22,11 +55,14 @@ function renderImg(imgUrl) {
 }
 function firstImgUrl() {
   const url = myImages[0].imgUrl;
+  myImages[myImages.length - 1].active = false;
+  myImages[0].active = true;
   return url;
 }
 function lastImgUrl() {
   const url = myImages[myImages.length - 1].imgUrl;
-
+  myImages[0].active = false;
+  myImages[myImages.length - 1].active = true;
   return url;
 }
 
@@ -59,15 +95,23 @@ function renderLastPhoto() {
 function findCurrentPhoto() {
   return frame.firstChild.attributes.src.nodeValue;
 }
+function findCurrentDiv() {
+  return frame.firstChild;
+}
 
 function next() {
   for (let i = 0; i < myImages.length; i += 1) {
     if (i === (myImages.length - 1)) {
       renderFirstPhoto();
+      makeActive();
     }
     if (myImages[i].imgUrl === findCurrentPhoto()) {
       console.log(myImages[i + 1].imgUrl);
       renderImg(myImages[i + 1].imgUrl);
+      myImages[i].active = false;
+      myImages[i + 1].active = true;
+      makeActive();
+      console.log(myImages);
       return;
     }
   }
@@ -76,10 +120,15 @@ function previous() {
   for (let i = myImages.length - 1; i > -1; i -= 1) {
     if (i === 0) {
       renderLastPhoto();
+      makeActive();
     }
     if (myImages[i].imgUrl === findCurrentPhoto()) {
       console.log(myImages[i - 1].imgUrl);
       renderImg(myImages[i - 1].imgUrl);
+      myImages[i].active = false;
+      myImages[i - 1].active = true;
+      makeActive();
+      console.log(myImages);
       return;
     }
   }
@@ -92,4 +141,7 @@ previousBtn.addEventListener('click', () => {
 });
 renderFirstPhoto();
 findCurrentPhoto();
+appendBullets();
+findCurrentDiv();
+makeActive();
 // next();
